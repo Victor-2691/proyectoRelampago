4aae61b3bc89153d267d4dad76cdddf332e0e967
Szuniga29 <steven.zuniga.araya@cuc.cr>
2023-04-01 16:12:30 -0600

    modelos pt2

---
+++ Capa_Negocios/Características.cs
@@ -0,0 +1,128 @@
+﻿using capa_datos;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocios
+{
+    public class Características
+    {
+        #region Caracteristicas
+
+        public void agregarCaracteristica(string carac)
+        {
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Caracteristica new_caracteristica = new Caracteristica();
+                    new_caracteristica.caracteristica1 = carac;
+
+                    db.Caracteristicas.Add(new_caracteristica);
+                    db.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        public DataTable verCaracteristicas()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    var caracteristicas = from d in db.Caracteristicas
+                                select d;
+                    dt = ConvertirListaToDataTable(caracteristicas.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public void modificarCaracteristica(int id, string carac)
+        {
+            try
+            {
+                using(tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Caracteristica new_caracteristica = new Caracteristica();
+                    new_caracteristica.Id_caracteristica = id;
+                    new_caracteristica.caracteristica1 = carac;
+
+                    db.Entry(new_caracteristica).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void eliminarCaracteristica(int id, string caracteristica)
+        {
+            try
+            {
+                using(tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Caracteristica new_caracteristica = new Caracteristica();
+                    new_caracteristica = db.Caracteristicas.Find(id);
+
+                    db.Caracteristicas.Remove(new_caracteristica);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region MetodosInternos
+
+        private DataTable ConvertirListaToDataTable(IList data)
+        {
+
+            var properties = TypeDescriptor.GetProperties(typeof(Idea));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (Idea item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties) row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        #endregion
+    }
+}
---
+++ Capa_Negocios/Ideas.cs
@@ -0,0 +1,131 @@
+﻿using capa_datos;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocios
+{
+    public class Ideas
+    {
+        #region Ideas
+
+        public void AgregarIdea(string idea)
+        {
+            using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+            {
+                try
+                {
+
+                    Idea new_idea = new Idea();
+                    new_idea.idea1 = idea;
+
+                    db.Ideas.Add(new_idea);
+                    db.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public DataTable VerIdeas()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    var lista = from d in db.Ideas
+                                select d;
+                    dt = ConvertirListaToDataTable(lista.ToList());
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public void ModificarIdeas(int idIdea, string idea)
+        {
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Idea new_idea = new Idea();
+                    new_idea.Id_idea = idIdea;
+                    new_idea.idea1 = idea;
+
+                    db.Entry(new_idea).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void EliminarIdeas(int idIdea)
+        {
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Idea new_idea = new Idea();
+                    new_idea = db.Ideas.Find(idIdea);
+
+                    db.Ideas.Remove(new_idea);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region MetodosInternos
+
+        private DataTable ConvertirListaToDataTable(IList data)
+        {
+
+            var properties = TypeDescriptor.GetProperties(typeof(Idea));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (Idea item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties) row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        #endregion
+    }
+}
