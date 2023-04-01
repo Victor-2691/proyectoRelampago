6aeb3bd59c66a3b9c0a88e600fb229db439acff1
Szuniga29 <stevenza.129@gmail.com>
2023-04-01 02:34:51 -0600

    métodos

--- CapaNegocios/Class1.cs
+++ CapaNegocios/Class1.cs
@@ -1,21 +1,165 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CapaDatos;
+using Microsoft.Win32;
 
 namespace CapaNegocios
 {
-    internal class Class1
+    public class Class1
     {
+
+        #region Iniciar Sesion
+
+        public bool Login(string user, string pass)
+        {
+            try
+            {
+                using(tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario = db.Usuarios.Find(user);
+                    usuario = db.Usuarios.Find(pass);
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Caracteristicas
+
+
+
+        #endregion
+
+
+        #region Ideas
+
         public void AgregarIdea(string idea)
         {
             using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
             {
-                Idea new_idea = new Idea();
-                new_idea.idea1 = idea;                
+                try
+                {
+
+                    Idea new_idea = new Idea();
+                    new_idea.idea1 = idea;
+
+                    db.Ideas.Add(new_idea);
+                    db.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                } 
             }
         }
+
+        public DataTable VerIdeas()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+
+                using(tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    var lista = from d in db.Ideas 
+                                select d;
+                    dt = ConvertirListaToDataTable(lista.ToList());
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public void ModificarIdeas(int idIdea, string idea)
+        {
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Idea new_idea = new Idea();
+                    new_idea.Id_idea = idIdea;
+                    new_idea.idea1 = idea;
+                    
+                    db.Entry(new_idea).State=System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void EliminarIdeas(int idIdea)
+        {
+            try
+            {
+                using (tiusr7pl_proyecto_relampagoEntities db = new tiusr7pl_proyecto_relampagoEntities())
+                {
+                    Idea new_idea = new Idea();
+                    new_idea = db.Ideas.Find(idIdea);
+
+                    db.Ideas.Remove(new_idea);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region MetodosInternos
+
+        private DataTable ConvertirListaToDataTable(IList data)
+        {
+
+            var properties = TypeDescriptor.GetProperties(typeof(Idea));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (Idea item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties) row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        #endregion
     }
 }
